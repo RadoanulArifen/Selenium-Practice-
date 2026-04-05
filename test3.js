d21f176e2f7cc90e8accb0feab273fd39078ffcf
Radoanul Arifen <radoanularifen@gmail.com>
2026-04-05 08:57:13 +0600

    Add initial implementation of testrun3 function in test3.js for browser interaction

---
+++ test3.js
@@ -0,0 +1,13 @@
+import {Builder, By, Browser, Key} from "selenium-webdriver";
+
+const driver = new Builder().forBrowser(Browser.CHROME).build();
+
+async function testrun3(){
+    await driver.get("https://demoqa.com/");
+    await driver.manage().window().maximize();
+    await driver.findElement(By.xpath("//h5[text()='Alerts, Frame & Windows']")).click();
+    await driver.findElement(By.xpath("//span[text()='Browser Windows']")).click();
+    await driver.findElement(By.id("tabButton")).click();
+
+}
+testrun3();
